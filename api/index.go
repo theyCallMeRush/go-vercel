99fcfd2258710c27b4253ce6091327a65fa79487
whoag <tarynthompson845@yahoo.com>
2022-11-13 16:41:24 -0500

    chat

--- api/index.go
+++ api/index.go
@@ -3,50 +3,8 @@ package handler
 import (
 	"github.com/gofiber/adaptor/v2"
 	"github.com/gofiber/fiber/v2"
-	"github.com/gofiber/websocket/v2"
-	"log"
 	"net/http"
 )
-
-type client struct{} // Add more data to this type if needed
-
-var clients = make(map[*websocket.Conn]client) // Note: although large maps with pointer-like types (e.g. strings) as keys are slow, using pointers themselves as keys is acceptable and fast
-var register = make(chan *websocket.Conn)
-var broadcast = make(chan string)
-var unregister = make(chan *websocket.Conn)
-
-func runHub() {
-	for {
-		select {
-		case connection := <-register:
-			clients[connection] = client{}
-			log.Println("connection registered")
-
-		case message := <-broadcast:
-			log.Println("message received:", message)
-
-			// Send the message to all clients
-			for connection := range clients {
-				if err := connection.WriteMessage(websocket.TextMessage, []byte(message)); err != nil {
-					log.Println("write error:", err)
-
-					err := connection.WriteMessage(websocket.CloseMessage, []byte{})
-					if err != nil {
-						return
-					}
-					connection.Close()
-					delete(clients, connection)
-				}
-			}
-
-		case connection := <-unregister:
-			// Remove the client from the hub
-			delete(clients, connection)
-
-			log.Println("connection unregistered")
-		}
-	}
-}
 
 // Handler is the main entry point of the application. Think of it like the main() method
 func Handler(w http.ResponseWriter, r *http.Request) {
@@ -72,49 +30,12 @@ func handler() http.HandlerFunc {
 		})
 	})
 
-	app.Static("/", "./home.html")
-
-	app.Use(func(c *fiber.Ctx) error {
-		if websocket.IsWebSocketUpgrade(c) { // Returns true if the client requested upgrade to the WebSocket protocol
-			return c.Next()
-		}
-		return c.SendStatus(fiber.StatusUpgradeRequired)
+	app.Get("/", func(ctx *fiber.Ctx) error {
+		return ctx.JSON(fiber.Map{
+			"uri":  ctx.Request().URI().String(),
+			"path": ctx.Path(),
+		})
 	})
 
-	go runHub()
-
-	app.Get("/ws", websocket.New(func(c *websocket.Conn) {
-		// When the function returns, unregister the client and close the connection
-		defer func() {
-			unregister <- c
-			err := c.Close()
-			if err != nil {
-				return
-			}
-		}()
-
-		// Register the client
-		register <- c
-
-		for {
-			messageType, message, err := c.ReadMessage()
-			if err != nil {
-				if websocket.IsUnexpectedCloseError(err, websocket.CloseGoingAway, websocket.CloseAbnormalClosure) {
-					log.Println("read error:", err)
-				}
-
-				return // Calls the deferred function, i.e. closes the connection on error
-			}
-
-			if messageType == websocket.TextMessage {
-				// Broadcast the received message
-				broadcast <- string(message)
-			} else {
-				log.Println("websocket message received of type", messageType)
-			}
-		}
-	}))
-
 	return adaptor.FiberApp(app)
-
 }
